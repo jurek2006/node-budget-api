141793dd4d4abf4a3a68f026843706892a9be286
Jurek <jurek2006@gmail.com>
2018-07-23 14:04:40 +0200

    Testowanie route POST /test

--- server/models/test.js
+++ server/models/test.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const Test = mongoose.model('Test', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,6 +2,11 @@ const expect = require('expect');
 const request = require('supertest');
 
 const {app} = require('./../server');
+const {Test} = require('./../models/test');
+
+beforeEach(done => {
+    Test.remove({}).then(() => done());
+});
 
 describe('GET /', () => {
     
@@ -12,4 +17,47 @@ describe('GET /', () => {
         .end(done);
     }); 
 
+});
+
+describe('POST /test', () => {
+
+    it('should create new test in database', done => {
+        const newText = 'Tekst testowy';
+        
+        request(app)
+        .post('/test')
+        .send({text: newText})
+        .expect(200)
+        .expect(res => {
+            expect(res.body.text).toBe(newText);
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+
+            Test.find().then(tests => {
+                expect(tests.length).toBe(1);
+                expect(tests[0].text).toBe(newText);
+                done();
+            }).catch(err => done(err));
+        });
+    });
+
+    it('should not create test in database when invalid data given', done => {
+        request(app)
+        .post('/test')
+        .send({})
+        .expect(400)
+        .end((err, red) => {
+            if(err){
+                return done(err);
+            }
+
+            Test.find().then(tests => {
+                expect(tests.length).toBe(0);
+                done();
+            }).catch(err => done(err));
+        });
+    });
 });
