8166bcd7c483b512dc8fc0cee0ff893a34270b36
Jurek <jurek2006@gmail.com>
2018-07-26 14:56:23 +0200

    Dodanie route POST /users - razem z JWT

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require('./config/config.js');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
 const {Test} = require('./models/test');
+const {User} = require('./models/user');
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,6 +28,22 @@ app.post('/test', (req, res) => {
         res.status(400).send(err);
     });
 
+});
+
+app.post('/users', (req, res) => {
+    const user = new User(_.pick(req.body, ['email', 'password']));
+
+    user.save()
+    .then(user => {
+        return user.generateAuthToken();
+    })
+    .then(token => {
+        res.header('x-auth', token).send(user);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).send(err);
+    });
 });
 
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -61,3 +61,4 @@ describe('POST /test', () => {
         });
     });
 });
+
